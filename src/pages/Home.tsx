cc0030ccfc13265cdcde1ed8efa2d435538d1484
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-06-24 20:41:04 -0300

    verificação se a sala foi fechada ou não

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,6 +38,11 @@ export function Home() {
       return;
     }
 
+    if (roomRef.val().endedAt) {
+      alert('Sala foi fechada pelo ADM');
+      return;
+    }
+
     history.push(`/rooms/${roomCode}`)
   }
 
